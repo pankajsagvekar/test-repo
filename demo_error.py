e47d2ad7bc401046845cd1b336a2b17ba3d7237e
Shivani Gulhane <shivanigulhane2002@gmail.com>
2026-02-19 23:22:57 +0530

    Update demo_error.py

--- demo_error.py
+++ demo_error.py
@@ -8,3 +8,4 @@ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 result = remove_even_numbers(my_list)
 print(f"Original list: {my_list}")
 print(f"List after removing even numbers: {result}")
+print(f"error remove from rohit's pc" {result}"
